b2dadce5faac29fbd07da21ed2a9551a75d6b225
realespee <wanyamasp@gmail.com>
2024-07-05 10:33:21 +0300

    Initial Commit

---
+++ cleardocspro/cleardocs_pro/doctype/clearpersonnel/clearpersonnel.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Simon Wanyama and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("ClearPersonnel", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ cleardocspro/cleardocs_pro/doctype/clearpersonnel/test_clearpersonnel.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Simon Wanyama and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestClearPersonnel(FrappeTestCase):
+	pass
